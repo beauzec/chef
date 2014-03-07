1d07fb0da74581a87a627e0be2899d12b397ebb2
beauzec <beauzec@gmail.com>
2014-03-07 11:35:21 +1000

    Chef Test 3

--- cookbooks/createfile/recipes/default.rb
+++ cookbooks/createfile/recipes/default.rb
@@ -3,35 +3,5 @@ file "/tmp/createfile.txt" do
   group "ubuntu"
   mode 00544
   action :create
-  content "{
-"staff":
-[
- {
- "firstName":"John",
- "lastName":"Parker",
- "age":"32",
- "country":"Canada",
- "job":"Programmer"
- },
-
- {
- "firstName":"Peter",
- "lastName":"Anderson",
- "age":"30",
- "country":"USA",
- "job":"System administrator"
- },
-
- {
- "firstName":"Bob",
- "lastName":"Johnson",
- "age":"35",
- "country":"Canada",
- "job":"Coder"
- }
-]
-}
-
-
-  "
+  content "It Worked!!!"
 end
